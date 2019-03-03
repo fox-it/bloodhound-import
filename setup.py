c1420fca544c1231f72cd795efd6fa96cdc8d4db
Matthijs Gielen <github@mwgielen.com>
2019-03-03 10:12:32 +0100

    Update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='bloodhound_import',
-      version='0.0.2',
+      version='0.0.3',
       description='BloodHound import from python',
       license='MIT',
       classifiers=[
